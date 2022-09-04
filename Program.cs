305feaa7f03ebcfb2eac9cedd04b75f472bb3440
anand acchu <you@example.com>
2022-09-04 23:16:42 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistration
+
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine(Addressbook.AddressBook)
+
+        }
+    }
+
+}
